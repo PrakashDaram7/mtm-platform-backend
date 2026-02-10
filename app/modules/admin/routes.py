b7122da5e7e8d2e9a3fe1d52a5fa699358a44268
PrakashDaram7 <prakash.daram@viprata.com>
2026-02-10 12:23:47 +0530

    add substraction for sample

--- app/modules/admin/routes.py
+++ app/modules/admin/routes.py
@@ -10,4 +10,7 @@ def addition(a, b):
     """Example function to demonstrate route handling."""
     return a + b
 
+def subtraction(a, b):
+    """Example function to demonstrate route handling."""
+    return a - b
 
