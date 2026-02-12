2a29c96a56eee680a01149b3feed379a8cf54f57
PrakashDaram7 <prakash.daram@viprata.com>
2026-02-12 17:05:21 +0530

    removed unnecessary stuff

--- app/modules/auth/routes.py
+++ app/modules/auth/routes.py
@@ -5,23 +5,3 @@ TODO:
 """
 pass
 
-
-def is_prime(n):
-    """Check if a number is prime.
-
-    Args:
-        n (int): The number to check."""
-    
-    if n <= 1:
-        return False
-    for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def setup_routes(app):
-    """Setup routes for the auth module.
-
-    Args:
-        app: The main application instance.
-    """
